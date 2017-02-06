837a28dbf90ac76e324ed8fc455f795ebe4a6fdd
Benjamin Borbe <bborbe@nova.local>
2017-02-06 15:28:04 +0100

    fix sources

--- build.sh
+++ build.sh
@@ -4,14 +4,14 @@ set -e
 
 make checkout
 
-tag=$(sources;git tag -l --points-at HEAD)
+tag=$(cd sources;git tag -l --points-at HEAD)
 if [ ! -z "$tag" ]; then
 	VERSION=${tag} make build
 	VERSION=${tag} make upload
 	VERSION=${tag} make clean || true
 fi
 
-branch=$(sources;git rev-parse --abbrev-ref HEAD)
+branch=$(cd sources;git rev-parse --abbrev-ref HEAD)
 if [ ! "$branch" = "HEAD" ]; then
 	VERSION=${branch} make build
 	VERSION=${branch} make upload
